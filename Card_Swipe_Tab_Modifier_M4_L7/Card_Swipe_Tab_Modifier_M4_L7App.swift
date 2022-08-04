dbcbe3f213246f41e8d38aaf90bf0989629b005e
David Devlin <david.devlin@theverygroup.com>
2022-08-04 10:04:40 +0100

    Initial Commit

---
+++ Card_Swipe_Tab_Modifier_M4_L7/Card_Swipe_Tab_Modifier_M4_L7App.swift
@@ -0,0 +1,17 @@
+//
+//  Card_Swipe_Tab_Modifier_M4_L7App.swift
+//  Card_Swipe_Tab_Modifier_M4_L7
+//
+//  Created by David Devlin on 04/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Card_Swipe_Tab_Modifier_M4_L7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Card_Swipe_Tab_Modifier_M4_L7/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Card_Swipe_Tab_Modifier_M4_L7
+//
+//  Created by David Devlin on 04/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
